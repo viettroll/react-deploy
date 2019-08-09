5972429d1fc92f04b21ffe102758f9a8c24836d6
VietZzz <ducviet.ictu.k11@gmail.com>
2019-08-09 10:16:47 +0700

    update modal image project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ModalImage from './components/modal-image/modal-image';
+import img8 from './images/08.jpg';
+import img9 from './images/09.jpg';
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Image Modal</h2>
+        <p>Click the image below to show the modal.</p>
+
+        <div 
+          style={{
+            display: `flex`
+          }}
+        >
+          <ModalImage
+            src={img8}
+            alt={`This is one of beautiful girls`}
+            ratio={`3:2`}
+          />
+          <ModalImage
+            src={img9}
+            alt={`This is also one of beautiful girls`}
+            ratio={`3:2`}
+          />
+        </div>
+
+        <div>
+          Made by <a href="https://about.phamvanlam.com/">Lam Pham</a>.
+          Visit me at <a href="https://completejavascript.com/">completejavascript.com</a>.
+        </div>
+      </div>
+    );
+  }
+}
